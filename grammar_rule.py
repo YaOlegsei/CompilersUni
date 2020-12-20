8c6e8888a9f89d00357cf23c0e5abae7a56d122c
Alexey <alexey.kolotilo@cruxlab.com>
2020-12-20 15:02:17 +0200

    Add removing disappearing NonTerminals

--- grammar_rule.py
+++ grammar_rule.py
@@ -1,4 +1,5 @@
-from typing import Set, Dict, List, Sequence, Optional
+from __future__ import annotations
+from typing import List, Sequence
 from grammar_symbol import GrammarSymbol, NonTerminal, Terminal
 
 
@@ -13,3 +14,60 @@ class GrammarRule:
             right_str += f" {symb}"
 
         return f"{self.left_symbol} ->{right_str}"
+
+    def remove_disappearing_from_rule(self, disappearing: Sequence[NonTerminal]) -> List[GrammarRule]:
+
+        def remove_disappearing_from_list(
+                right_symbols: List[GrammarSymbol],
+        ) -> List[List[GrammarSymbol]]:
+            i = 0
+            for symbol in right_symbols:
+                i += 1
+                if symbol in disappearing:
+                    last_parts = \
+                        remove_disappearing_from_list(right_symbols[i:])
+
+                    if len(last_parts) == 0:
+                        return [right_symbols[:i], right_symbols[:i - 1]]
+
+                    with_last_symb = list(
+                        map(
+                            lambda list: right_symbols[:i] + list,
+                            last_parts,
+                        )
+                    )
+
+                    no_last_symb = list(
+                        map(
+                            lambda list: right_symbols[:i - 1] + list,
+                            last_parts,
+                        )
+                    )
+
+                    return no_last_symb + with_last_symb
+
+            return [right_symbols]
+
+        def list_to_rule(symb_list) -> GrammarRule:
+            return GrammarRule(self.left_symbol, symb_list)
+
+        return list(
+            map(
+                list_to_rule,
+                remove_disappearing_from_list(self.right_symbols),
+            )
+        )
+
+
+if __name__ == "__main__":
+    rule = GrammarRule(
+        NonTerminal("A"),
+        [NonTerminal("B"),
+         Terminal("ast"),
+         NonTerminal("C"),
+         NonTerminal("V")]
+    )
+
+    new_rules = rule.remove_disappearing_from_rule([NonTerminal("C"), NonTerminal("B")])
+
+    print(new_rules)
